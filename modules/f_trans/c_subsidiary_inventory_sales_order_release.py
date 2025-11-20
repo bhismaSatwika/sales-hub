3f031efd69ead28128db660383d4b2e02a513595
Hariyo Koco <hariyokoco@gmail.com>
2025-11-20 15:29:38 +0700

    add create padf DO ini sales order release

--- modules/f_trans/c_subsidiary_inventory_sales_order_release.py
+++ modules/f_trans/c_subsidiary_inventory_sales_order_release.py
@@ -2,6 +2,7 @@ import base64
 from datetime import datetime, timedelta
 
 from fastapi import HTTPException, Request
+from fastapi.responses import StreamingResponse
 
 from library import *
 import os
@@ -215,7 +216,76 @@ class c_subsidiary_inventory_sales_order_release(object):
 
         # return data
 
+    
     async def create_pdf_do(self, id_trans):
+
+        sql_header = f"""SELECT
+                            ff.id_trans,
+                            ff.id_trans_sales_order,
+                            ff.tanggal_do,
+                            bb.id_company AS company_id,
+                            bb.company_name,
+                            cc.id_cabang AS cabang_id,
+                            cc.cabang_name,
+                            gg.id_customer AS customer_id,
+                            gg.nama_customer,
+                            gg.alamat,
+                            gg.no_ktp,
+                            gg.no_hp,
+                            gg.email,
+                            gg.account_va,
+                            gg.account_bank_name
+                        FROM
+                            trans_inventory_subsidiary_sales_order_header aa
+                            LEFT JOIN master_company bb ON aa.company_id = bb.id_company
+                            LEFT JOIN master_company_cabang cc ON aa.company_id = bb.id_company AND aa.cabang_id = cc.id_cabang
+                            LEFT JOIN master_user dd ON aa.salesman = dd.id_user
+                            LEFT JOIN master_jenis_pembayaran ee ON aa.id_pembayaran = ee.id_pembayaran
+                            LEFT JOIN trans_inventory_subsidiary_delivery_order ff ON aa.id_trans = ff.id_trans_sales_order
+                            LEFT JOIN master_customer gg ON aa.customer_id = gg.id_customer 	
+                        WHERE
+                            aa.id_trans = '{id_trans}'"""
+        
+        sql_detail = f"""SELECT
+                            dd.nama_produk,
+                            aa.qty,
+                            ee.uom_satuan,
+                            aa.harga_satuan,
+                            aa.harga_total,
+                            aa.pph_22_value,
+                            aa.ppn_value,
+                            aa.harga_total_ppn_pph 
+                        FROM
+                            trans_inventory_subsidiary_sales_order aa
+                            LEFT JOIN master_company bb ON aa.company_id = bb.id_company
+                            LEFT JOIN master_company_cabang cc ON aa.company_id = bb.id_company 
+                            AND aa.cabang_id = cc.id_cabang
+                            LEFT JOIN master_produk dd ON aa.produk_id = dd.id_produk
+                            LEFT JOIN master_produk_uom_satuan ee ON dd.uom_satuan = ee.id_uom_satuan
+                            LEFT JOIN trans_inventory_subsidiary_delivery_order ff ON aa.id_trans = ff.id_trans_sales_order 
+                        WHERE
+                            aa.id_trans = '{id_trans}'"""
+
+        result_header = await self.db.executeToDict(sql_header)
+        result_detail = await self.db.executeToDict(sql_detail)
+
+        data_header = result_header[0]
+        data_detail = result_detail
+        pdf = PDF(data_header, data_detail)
+
+        pdf_buffer = pdf.generate_report()
+        filenamex = data_header["id_trans"]
+
+        return StreamingResponse(
+            pdf_buffer,
+            media_type="application/pdf",
+            headers={"Content-Disposition": f"inline; filename={filenamex}.pdf"},
+        )
+
+
+
+    
+    async def create_pdf_do_old(self, id_trans):
         sql = f"""SELECT
                     aa.id_trans,
                     aa.id_trans_sales_order,
